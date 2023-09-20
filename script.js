bd065ac96af10144c82a910019cec8e418060beb
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2023-09-20 11:09:13 -0300

    Commit
    Number 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const emojis = ["😍","😍","❤","❤","🤐","🤐","😂","😂","😁","😁","🤬","🤬","🤑","🤑"];
+var shuf_emojis = emojis.sort( () => (Math.random() > .5) ? 2 : -1);
+
+for(var i = 0; i < emojis.length; i++){
+    let box = document.createElement('div');
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i];
+    document.querySelector('.game').appendChild(box);
+
+
+}
