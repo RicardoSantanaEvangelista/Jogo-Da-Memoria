c05db860c37c6c12608f2d24ba488ff9803c6cbc
RicardoSantanaEvangelista <ricardosantanaevangelista@gmail.com>
2023-09-20 11:41:42 -0300

    Commit
    Number 5

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-const emojis = ["😍","😍","❤","❤","🤐","🤐","😂","😂","😁","😁","🤬","🤬","🤑","🤑"];
+const emojis = ["😍","😍","❤","❤","🤐","🤐","😂","😂","😁","😁","🤬","🤬","🤑","🤑", "😎", "😎"];
 var shuf_emojis = emojis.sort( () => (Math.random() > .5) ? 2 : -1);
 
 for(var i = 0; i < emojis.length; i++){
     let box = document.createElement('div');
     box.className = 'item';
     box.innerHTML = shuf_emojis[i];
+
+    box.onclick = function(){
+        this.classList.add('boxOpen');
+        setTimeout(function(){
+            if(document.querySelectorAll('.boxOpen').length > 1){
+                if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
+                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
+                   
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+
+                    if(document.querySelectorAll('.boxMatch').length == emojis.length){
+                        alert('YOU WIN')
+                    }
+                }else{
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+
+                }
+            }
+        },500)
+    }
+
+
     document.querySelector('.game').appendChild(box);
 
 
